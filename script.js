332afe9e4ae848eef6d2a1d378da8201fadc92e9
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-05 16:27:06 +0530

    added voice change functionality

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let speech = new SpeechSynthesisUtterance();
 
+// change voice
+let voices = [];
+
+let selectedVoice = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = ()=> {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    // display each voices in dropbar
+    voices.forEach((voice, i)=> (selectedVoice.options[i] = new Option(voice.name, i)));
+};
+
+selectedVoice.addEventListener("change", ()=> {
+    speech.voice = voices[selectedVoice.value];
+});
+
+
+// speech functionality
 document.querySelector("button").addEventListener("click", ()=> {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
